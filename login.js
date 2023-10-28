020c5f2a5455ad44b6163e5f6a3a4dbfeaab292d
David Kartchner <dkartchner3@gmail.com>
2023-10-27 19:17:32 -0600

    Added username to each page from localstorage

--- login.js
+++ login.js
@@ -6,6 +6,7 @@ async function login() {
             resolve('success')
         }, 2000)
     })
+    loggedIn = true
     window.location.href = "index.html";
 }
 
@@ -13,3 +14,18 @@ function saveUsername() {
     const username = document.querySelector("#username");
     localStorage.setItem("userName", username.value);
 }
+
+function getUsername() {
+        let userName = localStorage.getItem("userName")
+        if (userName == null) userName = 'guest'
+        return userName
+  }
+
+function loadUsername() {
+    console.log('loadUsername hit!')
+    let userNameSpan = document.getElementById('userNameSpan')
+    userNameSpan.innerText = getUsername()
+}
+
+addEventListener("load", (event) => {});
+onload = (event) => loadUsername();
