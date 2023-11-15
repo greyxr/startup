e3a1c90d5a99dedfa310aa6569b0bc94fa92730e
David Kartchner <dkartchner3@gmail.com>
2023-11-14 21:02:46 -0700

    Fixed port issue

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const database = require('./database.js')
 const app = express();
 
 // The service port. In production the front-end code is statically hosted by the service on the same port.
-const port = process.argv.length > 2 ? process.argv[2] : 3000;
+const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
 // JSON body parsing using built-in middleware
 app.use(express.json());
